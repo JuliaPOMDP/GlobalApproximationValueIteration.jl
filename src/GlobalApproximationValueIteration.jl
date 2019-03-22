02ff000f1ce06244830093c88e3bba273ead68e4
Shushman <shushmanchoudhury@gmail.com>
2019-03-22 11:41:12 -0700

    Added exports

--- src/GlobalApproximationValueIteration.jl
+++ src/GlobalApproximationValueIteration.jl
@@ -11,7 +11,26 @@ using POMDPPolicies
 using POMDPModels
 using POMDPModelTools
 
+export
+    GlobalFunctionApproximator,
+    fit!,
+    compute_value,
+    LinearGlobalFunctionApproximator,
+    NonlinearGlobalFunctionApproximator
+
+export
+    GlobalApproximationValueIterationSolver,
+    GlobalApproximationValueIterationPolicy,
+    compute_featurevector,
+    solve,
+    action,
+    value,
+    action_value
+
 
 include("global_function_approximation.jl")
+include("linear_gfa.jl")
+include("nonlinear_gfa.jl")
+include("global_approximation_vi.jl")
 
 end # module
